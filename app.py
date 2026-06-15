190f757e644ea0d2587dc1ba10ee219b44fedba8
Alfa Marine <alfamarine.ferreira@gmail.com>
2026-06-14 21:54:27 -0300

    Remove reticências

--- app.py
+++ app.py
@@ -361,7 +361,7 @@ class TelaBoasVindas(QWidget):
         btn_nova = QPushButton("Nova Imagem")
         btn_nova.setFixedSize(130, 40)
 
-        btn_abrir = QPushButton("Abrir imagem…")
+        btn_abrir = QPushButton("Abrir imagem")
         btn_abrir.setFixedSize(130, 40)
         btn_abrir.clicked.connect(self.janela.abrir_imagem)
 
@@ -652,10 +652,9 @@ class JanelaPrincipal(QMainWindow):
         acao_nova_aba.triggered.connect(self.abrir_nova_aba)
         menu_arquivo.addSeparator()
         
-        acao_abrir = menu_arquivo.addAction("Abrir imagem…")
+        acao_abrir = menu_arquivo.addAction("Abrir imagem")
         acao_abrir.triggered.connect(self.abrir_imagem)
 
-        # NOVO: Adicionar opção de captura
         acao_camera = menu_arquivo.addAction("Capturar da Câmera")
         acao_camera.setShortcut("Ctrl+K")
         acao_camera.triggered.connect(self.capturar_da_camera)
@@ -663,7 +662,7 @@ class JanelaPrincipal(QMainWindow):
         acao_colar = menu_arquivo.addAction("Colar do clipboard")
         acao_colar.triggered.connect(self.colar_imagem_clipboard)
 
-        acao_salvar = menu_arquivo.addAction("Salvar imagem…")
+        acao_salvar = menu_arquivo.addAction("Salvar imagem")
         acao_salvar.triggered.connect(self.salvar_imagem)
         menu_arquivo.addSeparator()
         acao_sair = menu_arquivo.addAction("Sair")
